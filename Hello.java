7d949f86fa2a0dcc8c245e082f0f02663f7ebccb
afmzakaria <afmzakaria@gmail.com>
2019-03-17 22:49:52 +0600

    Added a new method called greetings to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 Public class Hello{
     Public static void main(String[] args){
         System.out.println("Vs Code is Awesome...");
+        greeting("A F M Zakaria")
+    }
+
+    Public static void greeting(String name){
+        System.out.println("Welcome "+name);
     }
 }
